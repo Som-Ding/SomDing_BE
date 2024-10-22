44ba44297cd02e5008e92c50a7813c92a9352628
mashin2002 <mashin0128@gmail.com>
2024-10-22 13:44:38 +0900

    refactor: ProjectResponseDTO -> remove orderId

--- src/main/kotlin/com/swcontest/somding/model/dto/response/ProjectResponseDTO.kt
+++ src/main/kotlin/com/swcontest/somding/model/dto/response/ProjectResponseDTO.kt
@@ -2,9 +2,11 @@ package com.swcontest.somding.model.dto.response
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.swcontest.somding.model.entity.enums.ProjectCategory // Import your enum class for ProjectCategory
+import lombok.AllArgsConstructor
 import java.time.LocalDate
 
 
+@AllArgsConstructor
 data class ProjectResponseDTO(
         val projectId: Long,
         val title: String,
@@ -13,7 +15,7 @@ data class ProjectResponseDTO(
         val targetPrice: Int,
         val gatherPrice: Int,
         val price: Int,
-        @JsonInclude(JsonInclude.Include.NON_NULL)
-        val orderId: Long?
+//        @JsonInclude(JsonInclude.Include.NON_NULL)
+//        val orderId: Long?
 )
 
